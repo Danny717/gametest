b6b7240023b3a6b1bc1d02f64aadefb62400cae9
Danny <drillll777@gmail.com>
2020-08-20 15:41:23 +0300

    init

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$items = [
+    'iron' => ['percent'=> 40, 'weight' => 75],
+    'copper' => ['percent'=> 30, 'weight' => 75],
+    'gold' => ['percent'=> 15, 'weight' => 150],
+    'platinum' => ['percent'=> 10, 'weight' => 500],
+    'diamond' => ['percent'=> 5, 'weight' => 1000],
+];
