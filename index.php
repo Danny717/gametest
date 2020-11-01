c2fd832ee3cbdec3c45414eac8004f4a8e26a144
Danny <drillll777@gmail.com>
2020-11-01 11:36:21 +0200

    new test

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
-$items = [
+/*$items = [
     'iron' => ['percent'=> 40, 'weight' => 75],
     'copper' => ['percent'=> 30, 'weight' => 75],
     'gold' => ['percent'=> 15, 'weight' => 150],
     'platinum' => ['percent'=> 10, 'weight' => 500],
     'diamond' => ['percent'=> 5, 'weight' => 1000],
 ];
+require_once "Resources.php";
+$resources = new Resources($items);
+$resources->addToCart();*/
+
